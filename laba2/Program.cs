4a036fa88cc043c4fae281dc444ef442536fe14b
grishanas <80056655+grishanas@users.noreply.github.com>
2021-03-10 23:37:26 +0300

    Revert "Revert "0""
    This reverts commit dcc74d5400e9914a0475935b1ba32a89e2d4e989.

--- laba2/Program.cs
+++ laba2/Program.cs
@@ -10,76 +10,133 @@ namespace NETTEST1
     {
         static void Main()
         {
-            byte[] recivebuf = new byte[200];
-            
+
+            bool anyResponse=false;
             //создание ip адресов выхода и входа
             Stopwatch track = new Stopwatch();
             IPAddress ipdest = IPAddress.Parse(Console.ReadLine());
             IPEndPoint endp = new IPEndPoint(ipdest, 1);
             IPEndPoint src = new IPEndPoint(IPAddress.Any, 1);
-            Socket icmp_s = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
-            icmp_s.ReceiveTimeout = 1000;
-            
+
+            Random rnd = new Random();
+            ushort value = (ushort)rnd.Next();
+            byte[] icmpData = new byte[40];
+            icmpData[0] = (byte)8;
+            icmpData[1] = (byte)0;
 
 
+
+            IPEndPoint pingDestination = new IPEndPoint(ipdest, 0);
 
             short TTL = 1;
-           
+            Socket icmp_s = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            icmp_s.ReceiveTimeout = 1000;
+            icmp_s.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, TTL);
+            icmp_s.Bind(src);
+
+            
+            ushort checksumValue;
+            IPHostEntry name = new IPHostEntry();
+            var recivebuf = new byte[200];
             while (TTL < 127)
             {
+                Console.Write("{0}", TTL);
                 try
                 {
-                    // создание сокета с icmp протоколом(ip заголовок)
-                    
-                                 
-
-                    //заполнение данных  icmp протокола
-                    byte[] icmpData = new byte[40];
-                    int offset = 0;
-                    icmpData[offset++] = (byte)8; 
-                    icmpData[offset++] = (byte)0; 
-                    icmpData[offset++] = 0;       
-                    icmpData[offset++] = 0;
-                    ushort checksumValue;
-
+                    icmpData[2] = 0;
+                    icmpData[3] = 0;
+                    icmpData[6] = (byte)(value);
+                    icmpData[7] = (byte)(value >> 8);
 
                     checksumValue = ComputeChecksum(icmpData);
-                  
 
-                    icmpData[2] =(byte) (checksumValue);
-                    icmpData[3] = (byte)(checksumValue>>8);
+                    icmpData[2] = (byte)(checksumValue);
+                    icmpData[3] = (byte)(checksumValue >> 8);
 
-                    IPEndPoint pingDestination = new IPEndPoint(ipdest, 0);
 
-                    icmp_s.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, TTL);
-                    icmp_s.Bind(src);
+            //        Socket icmp_s = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            //        icmp_s.ReceiveTimeout = 10000;
+                      icmp_s.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, TTL);
+                    //        icmp_s.Bind(src);
+                    anyResponse = false;
                     for (int i = 0; i < 3; i++)
                     {
-                        track.Reset();
-                        track.Start();
-                        icmp_s.SendTo(icmpData, pingDestination);
-                        var tmp = icmp_s.Receive(recivebuf);
-                        track.Stop();
-                        if(track.ElapsedMilliseconds>0)
+
+
+                        try
                         {
-                            Console.Write("  {0,3} ms  ", track.ElapsedMilliseconds);
+                            track.Reset();
+                            
+                            track.Start();
+                            icmp_s.SendTo(icmpData, pingDestination);
+                            var tmp = icmp_s.Receive(recivebuf);
+                            track.Stop();
+                            anyResponse = true;
+                            if (track.ElapsedMilliseconds > 0)
+                            {
+                                Console.Write("  {0,3} ms  ", track.ElapsedMilliseconds);
+                            }
+                            else
+                            {
+                                Console.Write("  <1 ms   ");
+                            }
+                            value = (ushort)((recivebuf[54] << 8) + recivebuf[53] + 1);
+
+                            icmpData[2] = 0;
+                            icmpData[3] = 0;
+                            icmpData[6] = (byte)(value);
+                            icmpData[7] = (byte)(value >> 8);
+
+                            checksumValue = ComputeChecksum(icmpData);
+
+                            icmpData[2] = (byte)(checksumValue);
+                            icmpData[3] = (byte)(checksumValue >> 8);
+
+
+
+
                         }
-                        else
+                        catch (SocketException e)
                         {
-                            Console.Write("  <1 ms   ");
+                            Console.Write("    *    ");
                         }
+
+
+
+
 
                     }
-                    
-                    int HeaderLength = (recivebuf[0] & 0xf)*4;
-                    
-                    Console.Write(recivebuf[12] + "."+ recivebuf[13] + "."+recivebuf[14] + "."+recivebuf[15]+"   ");
 
-                    IPHostEntry name = Dns.GetHostEntry(recivebuf[12] + "." + recivebuf[13] + "." + recivebuf[14] + "." + recivebuf[15]);
+                    // icmp_s.Close();
+                    if (anyResponse)
+                    {
+                        int HeaderLength = (recivebuf[0] & 0xf) * 4;
 
-                    Console.WriteLine(name.HostName);
-                    if (recivebuf[HeaderLength] == 0)
-                        break;
+
+
+                        Console.Write(recivebuf[12] + "." + recivebuf[13] + "." + recivebuf[14] + "." + recivebuf[15] + "   ");
+
+
+                        try
+                        {
+
+                            name = Dns.GetHostEntry(recivebuf[12] + "." + recivebuf[13] + "." + recivebuf[14] + "." + recivebuf[15]);
+                            Console.WriteLine(name.HostName);
+                        }
+                        catch (SocketException e)
+                        {
+                            Console.WriteLine("этот хост неизвестен");
+                        }
+
+
+                        if (recivebuf[HeaderLength] == 0)
+                            break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("адрес не найден") ;
+                        
+                    }
                 }
                 catch (SocketException e)
                 {
@@ -87,10 +144,13 @@ namespace NETTEST1
                 }
                 finally
                 {
+                   
                     ++TTL;
                 }
-            }
 
+
+            }
+            Console.WriteLine("Трассировка завершена.");
             Console.ReadKey();
         }
 
@@ -98,7 +158,7 @@ namespace NETTEST1
         {
 
             UInt32 chcksm = 0;
-            
+
             int index = 0;
 
             while (index < payLoad.Length)
